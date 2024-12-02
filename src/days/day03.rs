a7830c28b36071ddb36bf87f0dad24228fe18551
Alyx BB <git@alyxbb.tech>
2024-12-02 14:05:22 +0000

    initial commit

---
+++ src/days/day03.rs
@@ -0,0 +1,11 @@
+use crate::Solution;
+
+pub fn part_1(input: String) -> Solution {
+    let sol = 0;
+    Solution::from(sol)
+}
+
+pub fn part_2(input: String) -> Solution {
+    let sol = 0;
+    Solution::from(sol)
+}
