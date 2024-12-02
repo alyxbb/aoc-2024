d5792d7af6a2e78cb69d363cd12595622ccb85d4
Alyx BB <git@alyxbb.tech>
2024-12-02 17:34:07 +0000

    fix: warnings

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,11 +1,11 @@
 use crate::Solution;
 
-pub fn part_1(input: String) -> Solution {
+pub fn part_1(_input: String) -> Solution {
     let sol = 0;
     Solution::from(sol)
 }
 
-pub fn part_2(input: String) -> Solution {
+pub fn part_2(_input: String) -> Solution {
     let sol = 0;
     Solution::from(sol)
 }
