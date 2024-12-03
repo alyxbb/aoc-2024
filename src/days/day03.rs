d5336597c8e77d99c741d8e3bfab7b72985d5ffa
Alyx BB <git@alyxbb.tech>
2024-12-03 06:05:11 +0000

    day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,11 +1,37 @@
+use regex::Regex;
+
 use crate::Solution;
 
-pub fn part_1(_input: String) -> Solution {
-    let sol = 0;
+pub fn part_1(input: String) -> Solution {
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let captures = regex.captures_iter(&input);
+    let mut sol = 0;
+
+    for capture in captures {
+        sol += capture.get(1).unwrap().as_str().parse::<usize>().unwrap() * capture.get(2).unwrap().as_str().parse::<usize>().unwrap();
+    }
     Solution::from(sol)
 }
 
-pub fn part_2(_input: String) -> Solution {
-    let sol = 0;
+pub fn part_2(input: String) -> Solution {
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)|do(n't)?\(\)").unwrap();
+    let captures = regex.captures_iter(&input);
+
+    let mut sol = 0;
+    let mut enabled = true;
+
+    for capture in captures {
+        match &capture.get(0).unwrap().as_str()[..3] {
+            "do(" => enabled = true,
+            "don" => enabled = false,
+            "mul" => {
+                if enabled {
+                    sol += capture.get(1).unwrap().as_str().parse::<usize>().unwrap() * capture.get(2).unwrap().as_str().parse::<usize>().unwrap();
+                }
+            }
+            _ => panic!()
+            
+        }
+    }
     Solution::from(sol)
 }
